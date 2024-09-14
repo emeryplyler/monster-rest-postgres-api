63ad3db39cd5f2ee042193f7364f0d03dd580bf5
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2024-09-14 15:29:22 -0700

    post, get, get one, put finished for family

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const port = 8080;
 const mfamilyQuery = `
     CREATE TABLE IF NOT EXISTS MonsterFamilies(
         FamilyId SERIAL PRIMARY KEY,
-        FamilyName NVARCHAR(255) NOT NULL,
-        FamilyDescription NVARCHAR(255) NULL
+        FamilyName VARCHAR(255) NOT NULL,
+        FamilyDescription VARCHAR(255) NULL
     );
 `;
 
 const monsterQuery = `
     CREATE TABLE IF NOT EXISTS Monsters (
         MonsterId int SERIAL PRIMARY KEY,
-        Name NVARCHAR(255) NOT NULL,
-        Description nvarchar(500) null,
+        Name VARCHAR(255) NOT NULL,
+        Description VARCHAR(500) null,
         PowerLevel float null,
-        ImageUrl nvarchar(255) null,
-        Habitat nvarchar(255) null,
+        ImageUrl VARCHAR(255) null,
+        Habitat VARCHAR(255) null,
         FamilyId int FOREIGN KEY REFERENCES MonsterFamilies(FamilyId) null,
         IsLegendary bit null,
     );
@@ -27,8 +27,8 @@ const monsterQuery = `
 const userQuery = `
     CREATE TABLE IF NOT EXISTS Users(
         UserId SERIAL PRIMARY KEY,
-        Username NVARCHAR(50) NOT NULL,
-        DisplayName NVARCHAR(50) NOT NULL
+        Username VARCHAR(50) NOT NULL,
+        DisplayName VARCHAR(50) NOT NULL
     );
 `;
 
@@ -54,18 +54,104 @@ async function createTables(query) {
     // const query = mfamilyQuery;
 
     await pool.query(query);
-    console.log("Table created!");
+    console.log("Table is ready");
   } catch (err) {
     console.error(err);
-    console.error("Error: couldn't create table");
+    console.error("Couldn't create table");
   }
 }
 
-createTables(mfamilyQuery); // try creating query
+createTables(mfamilyQuery); // make sure family table exists
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Monster API v1.1");
 });
+
+// Monster Family Methods:
+
+app.get("/monsterfamilies", async (req, res) => {
+  try {
+    const query = "SELECT * FROM monsterFamilies;";
+    const { rows } = await pool.query(query);
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Get failed");
+  }
+});
+
+app.get("/monsterfamilies/:familyId", async (req, res) => {
+  try {
+    const { familyId } = req.params;
+    const query = `SELECT * FROM MonsterFamilies WHERE familyId = $1;`;
+    const { rows } = await pool.query(query, [familyId]);
+
+    if (rows.length == 0) {
+      return res.status(404).send("Family not found in the database");
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Get failed");
+  }
+});
+
+app.post("/monsterfamilies", async (req, res) => {
+  // validate
+  const { name, desc } = req.body;
+  console.log(req.body);
+  if (!name) {
+    return res.status(400).send("Name required");
+  }
+
+  try {
+    const query = `
+      INSERT INTO MonsterFamilies (FamilyName, FamilyDescription)
+      VALUES ($1, $2)
+      RETURNING FamilyId;
+    `;
+    const values = [name, desc];
+    const result = await pool.query(query, values);
+    res
+      .status(201)
+      .send({ message: "New family created", familyId: result.rows[0].id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Unable to enter data into family table");
+  }
+});
+
+app.put("/monsterfamilies/:familyId", async (req, res) => {
+  try {
+    const { familyId } = req.params;
+    const { name, desc } = req.body;
+
+    if (!name) {
+      return res.status(400).send("Name required");
+    }
+
+    const query = `
+      UPDATE MonsterFamilies
+      SET FamilyName = COALESCE($1, FamilyName),
+          FamilyDescription = COALESCE($2, FamilyDescription)
+      WHERE FamilyId = $3
+      RETURNING *;
+    `;
+    const { rows } = await pool.query(query, [name, desc, familyId]);
+
+    if (rows.length == 0) {
+      return res.status(404).send("Couldn't find a family with that id");
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Put failed");
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
